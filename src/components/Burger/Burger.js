6fe8158d885b14d0183e75e4f3ac2836645fc716
Michael Zick <michaelzick@gmail.com>
2018-01-01 19:11:22 +0000

    BURGER: reducing ingr and accounting for 0

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
             .map(ingKey => {
                 return [...Array(props.ingredients[ingKey])]
                     .map((_, i) => <BurgerIngredient key={ingKey + i} type={ingKey} />);
-            });
+            })
+            .reduce((arr, el) => {
+                return arr.concat(el);
+            }, []);
+
+        if (!transformedIngredients.length) {
+            transformedIngredients = <p>Please add some ingredients</p>;
+        }
 
     return (
         <div className={classes.Burger}>
