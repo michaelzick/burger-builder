e1409e63b218c72d35fd3ed8781913617ce8fa8c
Michael Zick <michaelzick@gmail.com>
2018-01-02 06:04:37 +0000

    MODAL: showing dynamic ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -87,8 +88,12 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal />
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>
+
                 <Burger ingredients={this.state.ingredients}/>
+
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
