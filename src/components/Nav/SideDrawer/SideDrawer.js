14ebb3cd63ad0112be60869e7f5c6a92c2ec1e18
Michael Zick <michaelzick@gmail.com>
2018-01-03 16:43:36 +0000

    LOGO: sidedrawer/toolbar logo styles

--- src/components/Nav/SideDrawer/SideDrawer.js
+++ src/components/Nav/SideDrawer/SideDrawer.js
@@ -6,7 +6,10 @@ import classes from './SideDrawer.css';
 const sideDrawer = () => {
     return (
         <div className={classes.SideDrawer}>
-            <Logo />
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
+
             <nav>
                 <NavItems />
             </nav>
