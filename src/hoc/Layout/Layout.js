56eb4e3350d125574f1564ba35295775c91d6bac
Michael Zick <michaelzick@gmail.com>
2018-01-06 06:24:36 +0000

    FOLDERS: moved layout and aux around

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../Aux/Aux';
 import classes from './Layout.css';
-import Toolbar from '../Nav/Toolbar/Toolbar';
-import SideDrawer from '../Nav/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Nav/Toolbar/Toolbar';
+import SideDrawer from '../../components/Nav/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
