a769fa00fe25c9bf7e260d77bef6f096f117fc5b
Michael Zick <michaelzick@gmail.com>
2018-01-06 00:03:43 +0000

    LIFECYCLE: added LC hooks in modal and order sum

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(ingKey => {
-            return (
-                <li key={ingKey}>
-                    <span style={{textTransform: 'capitalize'}}>{ingKey}</span>
-                    : {props.ingredients[ingKey]}
-                </li>
-            );
-        });
+class OrderSummary extends Component {
+    // Doesn't need to be class. For lifecycle hooks test.
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p>Total Price: {props.price.toFixed(2)}</p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    );
+    componentWillUpdate () {
+        console.log('order summary: componentWillUpdate');
+    }
+
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(ingKey => {
+                return (
+                    <li key={ingKey}>
+                        <span style={{textTransform: 'capitalize'}}>{ingKey}</span>
+                        : {this.props.ingredients[ingKey]}
+                    </li>
+                );
+            });
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p>Total Price: {this.props.price.toFixed(2)}</p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop
-            clicked={props.modalClosed}
-            show={props.show} />
-        <div className={classes.Modal}
-            style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? 1 : 0
-                  }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component {
+    shouldComponentUpdate (nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    componentWillUpdate () {
+        console.log('modal: componentWillUpdate');
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Backdrop
+                    clicked={this.props.modalClosed}
+                    show={this.props.show} />
+                <div className={classes.Modal}
+                    style={{
+                            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                            opacity: this.props.show ? 1 : 0
+                          }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
+}
+
+export default Modal;
