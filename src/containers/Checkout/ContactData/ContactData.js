5531d8dda07446cb362cd9126c4b3e70e24785d5
Michael Zick <michaelzick@gmail.com>
2018-02-03 18:33:21 +0000

    ORDER: added classes/css

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
+import classes from './ContactData.css';
 
 class ContactData extends Component {
     state = {
@@ -13,13 +14,13 @@ class ContactData extends Component {
 
     render () {
         return (
-            <div>
+            <div className={classes.ContactData}>
                 <h4>Enter your contact info</h4>
                 <form>
-                    <input type='text' name='name' placeholder='Name' />
-                    <input type='email' name='email' placeholder='Email' />
-                    <input type='text' name='street' placeholder='Street' />
-                    <input type='text' name='postal' placeholder='Postal Code' />
+                    <input className={classes.Input} type='text' name='name' placeholder='Name' />
+                    <input className={classes.Input} type='email' name='email' placeholder='Email' />
+                    <input className={classes.Input} type='text' name='street' placeholder='Street' />
+                    <input className={classes.Input} type='text' name='postal' placeholder='Postal Code' />
 
                     <Button btnType='Success'>ORDER</Button>
                 </form>
