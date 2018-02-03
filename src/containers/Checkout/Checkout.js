bee3e1fe123439a04423963e364a568f973ef628
Michael Zick <michaelzick@gmail.com>
2018-02-03 18:11:48 +0000

    ORDER: got order details rendering in route

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
@@ -39,6 +41,10 @@ class Checkout extends Component {
                     checkoutCancelled={this.checkoutCancelHandler}
                     checkoutContinued={this.checkoutContinuedHandler}
                     ingredients={this.state.ingredients} />
+
+                <Route
+                    path={this.props.match.path + '/contact-data'}
+                    component={ContactData} />
             </div>
         );
     }
