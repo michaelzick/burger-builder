4c44c35bc259092f97a90273cd1eda952ac4f5f7
Michael Zick <michaelzick@gmail.com>
2018-02-18 20:58:23 +0000

    FORM: added validation classes

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,19 +3,24 @@ import classes from './Input.css';
 
 const input = props => {
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
+
+    if (props.invalid && props.shouldValidate) {
+        inputClasses.push(classes.Invalid);
+    }
 
     switch (props.elementType) {
         case 'input':
             inputElement = <input
                             onChange={props.changed}
-                            className={classes.InputElement}
+                            className={inputClasses.join(' ')}
                             {...props.elementConfig}
                             value={props.value} />;
             break;
         case 'textarea':
             inputElement = <textarea
                             onChange={props.changed}
-                            className={classes.InputElement}
+                            className={inputClasses.join(' ')}
                             {...props.elementConfig}
                             value={props.value} />;
             break;
@@ -23,7 +28,7 @@ const input = props => {
             inputElement = (
                 <select
                     onChange={props.changed}
-                    className={classes.InputElement}
+                    className={inputClasses.join(' ')}
                     value={props.value}>
                     {props.elementConfig.options.map(option => (
                         <option
