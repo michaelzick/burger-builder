54cc9bc3d33ad898bdc5b9a88352807b8d5bfc15
Michael Zick <michaelzick@gmail.com>
2018-03-05 20:22:38 +0000

    AUTH: got auth form validating

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -69,6 +69,23 @@ class Auth extends Component {
         return isValid;
     }
 
+    inputChangedHandler = (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(
+                    event.target.value,
+                    this.state.controls[controlName].validation),
+                    touched: true
+            }
+        };
+        this.setState({
+            controls: updatedControls
+        });
+    }
+
     render () {
         const formElements = [];
 
