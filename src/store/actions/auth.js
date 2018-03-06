8086e0854470326435a1c6a7c487c9f651058426
Michael Zick <michaelzick@gmail.com>
2018-03-06 18:54:19 +0000

    AUTH: added err to authFail()

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -44,7 +44,7 @@ export const auth = (email, password, isSignUp) => {
             })
             .catch((err) => {
                 console.log(err);
-                dispatch(authFail());
+                dispatch(authFail(err));
             });
     };
 };
