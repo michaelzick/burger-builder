86171642368df1f9ea04786dccfa2f79aff7fd92
Michael Zick <michaelzick@gmail.com>
2018-03-07 01:49:13 +0000

    AUTH: see orders when authed

--- src/store/actions/orderActions.js
+++ src/store/actions/orderActions.js
@@ -62,11 +62,11 @@ export const fetchOrdersStart = () => {
     };
 };
 
-export const fetchOrders = () => {
+export const fetchOrders = (token) => {
     return (dispatch) => {
         dispatch(fetchOrdersStart());
 
-        axios.get('/orders.json')
+        axios.get('/orders.json?auth=' + token)
             .then(resp => {
                 const fetchedOrders = [];
 
