3956d9209120eb42544a09bba7f717b8a3849e3e
Michael Zick <michaelzick@gmail.com>
2018-03-08 05:55:16 +0000

    AUTH: added logout

---
+++ src/containers/Auth/Logout/Logout.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../../../store/actions/index';
+
+class Logout extends Component {
+    componentDidMount () {
+        this.props.onLogout(
+            this.props.history
+        );
+    }
+
+    render () {
+        return (
+            <Redirect to='/' />
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onLogout: () => dispatch(actions.logout())
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Logout);
